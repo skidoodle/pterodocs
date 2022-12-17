d1cc871db9867eda142bc8944d7c650e8a225daf
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-12-17 20:36:47 +0100

    * Change theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Pterodactyl Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/pterodactyl/',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/pterodactyl',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+  docsRepositoryBase: 'https://github.com/skidoodle/pterodocs',
+};
 
-export default config
+export default config;
